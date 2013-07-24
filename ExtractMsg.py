fdf4d85745a622f30ac7fa5653ad89a6c6e12ca5
Matthew Walker <mattgwwalker@gmail.com>
2013-07-24 16:08:15 +1200

    Added license

--- ExtractMsg.py
+++ ExtractMsg.py
@@ -1,3 +1,33 @@
+#!/usr/local/bin/python
+# -*- coding: latin-1 -*-
+"""
+ExtractMsg:
+    Extracts emails and attachments saved in Microsoft Outlook's .msg files
+
+version 0.1 2013-07-24 Matthew Walker - https://github.com/mattgwwalker/msg-extractor
+"""
+
+__author__  = "Matthew Walker"
+__date__    = "2013-07-24"
+__version__ = '0.1'
+
+#--- LICENSE ------------------------------------------------------------------
+#
+#    Copyright 2013 Matthew Walker
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import OleFileIO_PL as OleFile  # Used version 0.25 http://www.decalage.info/python/olefileio
 from email.parser import Parser as EmailParser
 import email.utils
