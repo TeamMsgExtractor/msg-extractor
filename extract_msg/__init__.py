f1abe453370d0e7e749e3f0acc37fd0df246c993
Ken Peterson <arceusthe@gmail.com>
2018-11-21 22:51:19 -0800

    Update __init__.py

--- extract_msg/__init__.py
+++ extract_msg/__init__.py
@@ -1,1 +1,12 @@
+"""
+extract_msg:
+    Extracts emails and attachments saved in Microsoft Outlook's .msg files
+
+https://github.com/mattgwwalker/msg-extractor
+"""
+
+__author__ = 'Matthew Walker & The Elemental of Creation'
+__date__ = '2018-05-22'
+__version__ = '0.20.1'
+
 from extract_msg.extract_msg import Attachment, Properties, Props, Recipient, Message, msg_epoch, fromdatetime, constants, parse_type, properHex
