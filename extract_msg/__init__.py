b044389c54e859c0c28c6265f850592007257f89
TheElementalOfCreation <arceusthe@gmail.com>
2018-11-22 20:43:50 -0800

    Version 0.20.2

--- extract_msg/__init__.py
+++ extract_msg/__init__.py
@@ -7,7 +7,7 @@ https://github.com/mattgwwalker/msg-extractor
 
 __author__ = 'Matthew Walker & The Elemental of Creation'
 __date__ = '2018-05-22'
-__version__ = '0.20.1'
+__version__ = '0.20.2'
 
 import glob
 import sys
