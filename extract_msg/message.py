1a00f09b2b04dba6a0d539527d6fc19c4b4b2fab
TheElementalOfCreation <arceusthe@gmail.com>
2019-01-06 12:35:51 -0800

    indentation error. Removed unwanted file.

--- extract_msg/message.py
+++ extract_msg/message.py
@@ -434,7 +434,7 @@ class Message(olefile.OleFileIO):
         except AttributeError:
             self._body = self._getStringStream('__substg1.0_1000')
             if self._body:
-				self._body = encode(self._body)
+                self._body = encode(self._body)
                 a = re.search('\n', self._body)
             if a is not None:
                 if re.search('\r\n', self._body) is not None:
