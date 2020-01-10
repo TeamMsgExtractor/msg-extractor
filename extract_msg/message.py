1b135bdbdfb13c76442584d4599081c064ff61e9
Victor Torre <web.ehooo@gmail.com>
2020-01-10 11:30:07 +0100

    improve exception management
    Fix #112

--- extract_msg/message.py
+++ extract_msg/message.py
@@ -45,7 +45,7 @@ class Message(olefile.OleFileIO):
             olefile.OleFileIO.__init__(self, path)
         except IOError as e:    # py2 and py3 compatible
             logger.error(e)
-            if e.message == 'not an OLE2 structured storage file':
+            if getattr(e, 'message', str(e)) == 'not an OLE2 structured storage file':
                 raise InvalidFileFormat(e)
             else:
                 raise
