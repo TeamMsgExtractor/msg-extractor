30a5b2643c1af3781cca8b83b4c9e507e9c571d9
TheElementalOfDestruction <arceusthe@gmail.com>
2022-06-23 23:26:52 -0700

    Fix issue with contact timezones

--- extract_msg/contact.py
+++ extract_msg/contact.py
@@ -734,8 +734,8 @@ class Contact(MessageBase):
                 'Email3 Display As': self.email3DisplayName,
             },
             '-other-': {
-                'Birthday': self.birthdayLocal.__format__('%B %d, %Y') if self.birthdayLocal else None,
-                'Anniversary': self.weddingAnniversaryLocal.__format__('%B %d, %Y') if self.weddingAnniversaryLocal else None,
+                'Birthday': self.birthday.__format__('%B %d, %Y') if self.birthdayLocal else None,
+                'Anniversary': self.weddingAnniversary.__format__('%B %d, %Y') if self.weddingAnniversaryLocal else None,
                 'Spouse/Partner': self.spouseName,
                 'Profession': self.profession,
                 'Children': ', '.join(self.childrensNames),
