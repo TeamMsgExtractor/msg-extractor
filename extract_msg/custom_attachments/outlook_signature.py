0e23a28936f205265d18df46412c1eccb20d16ef
TheElementalOfDestruction <arceusthe@gmail.com>
2022-11-30 20:09:27 -0800

    Started parsing for Ole Stream

--- extract_msg/custom_attachments/outlook_signature.py
+++ extract_msg/custom_attachments/outlook_signature.py
@@ -4,7 +4,8 @@ from .custom_handler import CustomAttachmentHandler
 from ..enums import DVAspect
 
 
-_MAILSTREAM_STRUCT = struct.Struct('<III')
+_ST_OLE = struct.Struck('<IIIII')
+_ST_MAILSTREAM = struct.Struct('<III')
 
 
 class OutlookSignature(CustomAttachmentHandler):
@@ -14,18 +15,31 @@ class OutlookSignature(CustomAttachmentHandler):
         stream = attachment._getStream('__substg1.0_3701000D/\x03MailStream')
         if not stream:
             raise ValueError('MailStream could not be found.')
+        if len(stream) != 12:
+            raise ValueError('MailStream is the wrong length.')
         # Next get the bitmap data.
         self.__data = attachment._getStream('__substg1.0_3701000D/CONTENTS')
         if not self.__data:
             raise ValueError('Bitmap data could not be read for Outlook signature.')
         # Get the OLE data.
+        oleStream = attachment._getStream('__substg1.0_3701000D/\x01Ole')
+        if not oleStream:
+            raise ValueError('OLE stream could not be found.')
+        if len(oleStream) != 20:
+            raise ValueError('OLE stream is the wrong length.')
+
+        # Unpack and verify the OLE stream.
+        vals = _ST_OLE.unpack(oleStream)
+        # Check the version magic.
+        if vals[0] != 0x20000001:
+            raise ValueError('OLE stream has wrong version magic.')
         # TODO.
 
         # Unpack the mailstream and create the HTML tag.
-        vals = _MAILSTREAM_STRUCT.unpack(stream)
+        vals = _ST_MAILSTREAM.unpack(stream)
         self.__dvaspect = DVAspect(vals[0])
         self.__x = vals[1]
         self.__y = vals[2]
-        hwStyle = f'height: {self.__x / 100.0:.2f}; width: {self.__y / 100.0:.2f};'
+        hwStyle = f'height: {self.__x / 100.0:.2f}mm; width: {self.__y / 100.0:.2f}mm;'
         imgData = f'data:image;base64,{base64.b64encode(self.__data)}';
         self.__htmlTag = f'<img src="{imgData}", style="{hwStyle}">'.encode('ascii')
