10dc680f6731352e43cccb49d1cf1de81e382456
TheElementalOfDestruction <arceusthe@gmail.com>
2022-12-08 00:53:46 -0800

    Progress/finished with HTML tokenizer

--- extract_msg/custom_attachments/utils.py
+++ extract_msg/custom_attachments/utils.py
@@ -5,14 +5,82 @@ submodule than for the main module.
 
 import bs4
 
+from typing import List
+
 
 def tokenizeHtml(html : str) -> List[str]:
     # Setup a few variables for state tracking.
     inTag = False
+    # Used for tracking escapes starting with &. If your escape ends up at 100
+    # characters because it is missing the semicolon, we are going to throw an
+    # error.
     inEscape = False
     inString = False
     # Only used when in string. Last character was a backslash.
     isBackslash = False
+    # Tells which quote type we are in.
+    isDoubleQuote = False
+
+    tokens = []
+    currentToken = ''
+
+    # Finally, let's start breaking things up. Our rules are that if we start a
+    # quote while in a tag, then we acknowledge it, otherwise it is treated as
+    # plain text.
+    for character in html:
+        # First we need to know our state, as our state determines what how we
+        # process a character.
+        if inTag:
+            currentToken += character
+            if inString:
+                if character == '"' and isDoubleQuote:
+                    # If isBackslash then we stay in the quote, otherwise...
+                    isQuote = isBackslash
+                elif character == "'" and not isDoubleQuote:
+                    # If isBackslash then we stay in the quote, otherwise...
+                    isQuote = isBackslash
+                elif character == '\\':
+                    isBackslash = not isBackslash
+                if character != '\\':
+                    isBackslash = False
+            else:
+                if character == '>':
+                    inTag = False
+                    tokens.append(currentToken)
+                    currentToken = ''
+        elif inEscape:
+            currentToken += character
+            if len(currentToken) > 99:
+                raise ValueError('Found escape that was too long (is a ; missing?)')
+            if character == ';':
+                tokens.append(currentToken)
+                currentToken = ''
+                inEscape = False
+        elif inString:
+            # This is an error. We should *never* be in a quote if we are not in
+            # a tag.
+            raise ValueError('Found to be inQuote when not in tag.')
+        else:
+            # We are currently processing plain text, so let's just handle.
+            if character == '&':
+                if currentToken:
+                    tokens.append(currentToken)
+                currentToken = character
+                inEscape = True
+            elif character == '<':
+                if currentToken:
+                    tokens.append(currentToken)
+                currentToken = character
+                inTag = True
+                inString = False
+            else:
+                currentToken += character
+
+    if currentToken:
+        tokens.append(currentToken)
+
+    return tokens
+
 
 def htmlSplitRendered(html : bytes) -> List[str]:
     """
