5f2448959862424b44e924595fde3f4771165ead
TheElementalOfDestruction <arceusthe@gmail.com>
2023-01-22 22:21:47 -0800

    Started work on functions for rtf tokenizer

--- extract_msg/rtf/rtf_tokenizer.py
+++ extract_msg/rtf/rtf_tokenizer.py
@@ -1,3 +1,10 @@
 class RTFTokenizer:
-    def __init__(data : bytes = None):
+    def __init__(self, data : bytes = None):
+
+        # Feed the data to our parser if provided. Feeding will clear all of the
+        # existing data.
+        if data:
+            self.feed(data)
+
+    def feed(self, data : bytes) -> None:
         pass
