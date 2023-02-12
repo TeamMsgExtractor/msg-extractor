3d8a300364202fc3f5ec31bb034de10f6c0cd173
TheElementalOfDestruction <arceusthe@gmail.com>
2023-02-12 15:08:57 -0800

    Progress on one form of RTF Tokenizer. Considering another

--- extract_msg/_rtf/rtf_tokenizer.py
+++ extract_msg/_rtf/rtf_tokenizer.py
@@ -24,6 +24,20 @@ class RTFTokenizer:
     def __iter__(self):
         return self.tokens.__iter__()
 
+    def __handleTag(self, tag : bytes, param : bytes) -> Token:
+        """
+        Handles converting an RTF tag into a Token.
+        """
+        if tag[1] == '*':
+            # Handle custom destination. We also need to handle bad destination
+            # data. TODO
+            pass
+        elif tag[1] in self.__knownSymbols:
+            pass
+
+        # TODO
+        pass
+
     def feed(self, data : bytes) -> None:
         """
         Reads in the bytes and sets the tokens list to the contents after
@@ -34,6 +48,7 @@ class RTFTokenizer:
         previous and not to the current one.
 
         :raises TypeError: The data is not recognized as RTF.
+        :raises ValueError: An issue with basic parsing occured.
         """
         reader = io.BytesIO(data)
         # This tokenizer *only* breaks things up. It does *not* care about
@@ -55,6 +70,97 @@ class RTFTokenizer:
         if nextChar != ' ':
             reader.seek(reader.tell() - 1)
 
+        # Setup the loop variables.
+        lastCharacter = b''
+        inTag = False
+        param = b''
+        raw = b''
+        # Tracking for if we are handling a `\'HH` tag.
+        isHex = False
+
+        while (currentChar := reader.read()) != b'':
+            if currentChar == b' ' and inTag:
+                # End the tag and drop the space.
+                tokens.append(self.__handleTag(raw, param))
+                raw = b''
+                param = b''
+                inTag = False
+            elif currentChar in (b'{', b'}'):
+                # Brackets are second fastest to parse, so do them next.
+                if inTag:
+                    if raw == b'\\':
+                        # If we only have the backslash, this is a symbol.
+                        tokens.append(self.__handleTag(b'\\' + currentChar, b''))
+                        raw = b''
+                        param = b''
+                        inTag = False
+                    else:
+                        # We already have a currentTag, so we need to push it
+                        # then push a group.
+                        tokens.append(self.__handleTag(raw, param))
+                        raw = b''
+                        param = b''
+                        inTag = False
+                        if currentChar = b'{':
+                            tokens.append(Token(currentChar, TokenType.GROUP_START))
+                        else:
+                            tokens.append(Token(currentChar, TokenType.GROUP_END))
+                else:
+                    if raw:
+                        tokens.append(Token(raw, TokenType.TEXT))
+                        raw = b''
+                    if currentChar = b'{':
+                        tokens.append(Token(currentChar, TokenType.GROUP_START))
+                    else:
+                        tokens.append(Token(currentChar, TokenType.GROUP_END))
+            elif currentChar == b'*' and inTag:
+                if len(raw) == 1:
+                    raw += b'*'
+                else:
+                    # End the current tag and start new text.
+                    tokens.append(self.__handleTag(raw, param))
+                    raw = b'*'
+                    param = b''
+                    inTag = False
+            elif currentChar == b'\\':
+                if inTag:
+                    if lastCharacter == b'\\':
+                        # If the current character is a backslash and we are in
+                        # a tag, then it is a backslash symbol and we need to
+                        # push it to the list.
+                        tokens.append(self.__handleTag(raw + b'\\', param))
+                        raw = b''
+                        param = b''
+                    elif lastCharacter == b'*':
+                        # This is a custom destination, but we aren't handling
+                        # that in this section, so just add to the current tag.
+                        raw += currentChar
+                    else:
+                        # We are starting a new tag.
+                        tokens.append(self.__handleTag(raw, param))
+                        raw = b'\\'
+                        param = b''
+                else:
+                    # If we were not already in a tag, check if we have text to
+                    # push, and if we do we push it. After that, start a tag.
+                    if raw:
+                        tokens.append()
+                    raw += b'\\'
+                    inTag = True
+            elif currentChar == b'\'' and inTag:
+                # If the current character is an apostrophe and we are in a tag,
+                # check if it is the first character after the backslash. If it
+                # is, then we are handling hex character, otherwise we are
+
+            elif currentChar.isalpha():
+                pass
+
+
+        lastCharacter = currentChar
+
+    # Since we are done, set the tokens list to the new one.
+    self.tokens = tokens
+
 
 
 class TokenType(enum.Enum):
@@ -63,6 +169,7 @@ class TokenType(enum.Enum):
     CONTROL = 2
     SYMBOL = 3
     DESTINATION = 4
+    TEXT = 5
 
 
 
