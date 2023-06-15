fca8951f219e9da1d6b4feca6880b1d5390e822f
TheElementalOfDestruction <arceusthe@gmail.com>
2023-06-15 14:42:30 -0700

    Update __init__ with current date

--- extract_msg/__init__.py
+++ extract_msg/__init__.py
@@ -27,7 +27,7 @@ https://github.com/TeamMsgExtractor/msg-extractor
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Destiny Peterson & Matthew Walker'
-__date__ = '2023-06-13'
+__date__ = '2023-06-15'
 __version__ = '0.42.0'
 
 __all__ = [
