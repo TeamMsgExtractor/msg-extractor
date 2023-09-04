974eadf83b91638d086b25613cd8d837bfd96c85
TheElementalOfDestruction <arceusthe@gmail.com>
2023-09-04 05:01:44 -0700

    typing and tobytes

--- extract_msg/structures/ole_stream_struct.py
+++ extract_msg/structures/ole_stream_struct.py
@@ -3,15 +3,17 @@ __all__ = [
 ]
 
 
-from typing import Optional
+from typing import final, Optional
 
+from ..constants import st
 from ._helpers import BytesReader
 from .mon_stream import MonikerStream
 
 
+@final
 class OleStreamStruct:
     def __init__(self, data : Optional[bytes] = None):
-        self.__reservedMonikerStream = None
+        self.__rms = None
         if not data:
             self.__flags = 0
             self.__linkUpdateOption = 0
@@ -24,11 +26,31 @@ class OleStreamStruct:
         reader.assertNull(4, 'Ole stream reserved was not null (got {actual}).')
         rmsSize = reader.readUnsignedInt()
         if rmsSize > 0:
-            self.__reservedMonikerStream = MonikerStream(reader.read(rmsSize))
+            self.__rms = MonikerStream(reader.read(rmsSize))
 
         # TODO implement the rest. It's all optional things.
 
     def toBytes(self) -> bytes:
-        pass # TODO
+        ret = b'\x01\x00\x00\x02'
+        ret += st.ST_LE_UI32.pack(self.__flags)
+        ret += st.ST_LE_UI32.pack(self.__linkUpdateOption)
+        ret += b'\x00\x00\x00\x00'
+        rmsBytes = b'' if self.__rms is None else self.__rms.toBytes()
+        ret += st.ST_LE_UI32.pack(len(rmsBytes)) + rmsBytes
+        # TODO finish this with the optional properties.
+
+        return ret
+
+    @property
+    def reservedMonikerStream(self) -> Optional[MonikerStream]:
+        """
+
+        """
+        return self.__rms
+
+    @reservedMonikerStream.setter
+    def setter(self, data : Optional[MonikerStream]) -> None:
+        if data is not None and not isinstance(data, MonikerStream):
+            raise TypeError('Reserved moniker stream must be a MonikerStream instance or None.')
         
 
