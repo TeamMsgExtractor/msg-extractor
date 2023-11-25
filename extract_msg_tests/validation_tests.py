b1aa63f09571c7a409a20a66bc1e71ec72b4e78a
TheElementalOfDestruction <arceusthe@gmail.com>
2023-11-25 11:18:49 -0800

    Constants validation

--- extract_msg_tests/validation_tests.py
+++ extract_msg_tests/validation_tests.py
@@ -32,6 +32,42 @@ class ValidationTests(unittest.TestCase):
                 with self.subTest(f'Ensure {name} is exported.'):
                     self.assertIn(name, constants.st.__all__)
 
+        # Ensure names are all uppercase.
+        for name in constants.st.__all__:
+            with self.subTest(f'Ensure {name} is a valid constant name.'):
+                self.assertTrue(name.isupper())
+
+        # Validate the regular expressions section.
+        # Check for exports that are not regular expressions.
+        for name in constants.re.__all__:
+            with self.subTest(f'Check that {name} is an instance of Pattern.'):
+                self.assertIsInstance(getattr(constants.re, name), re.Pattern)
+
+        # Check for regular expressions that didn't get exported.
+        for name in dir(constants.re):
+            if isinstance(getattr(constants.re, name), re.Pattern):
+                with self.subTest(f'Ensure {name} is exported.'):
+                    self.assertIn(name, constants.re.__all__)
+
+        # Ensure names are all uppercase.
+        for name in constants.re.__all__:
+            with self.subTest(f'Ensure {name} is a valid constant name.'):
+                self.assertTrue(name.isupper())
+
+        # The PropertiesSet section.
+        # Ensure names are all uppercase.
+        for name in constants.ps.__all__:
+            with self.subTest(f'Ensure {name} is a valid constant name.'):
+                self.assertTrue(name.isupper())
+
+        # Basic validation of the normal constants section. We have to do
+        # things a little differently since the submodule are lowercase.
+        # Ensure names are all uppercase.
+        for name in constants.__all__:
+            with self.subTest(f'Ensure {name} is a valid constant name.'):
+                if not isinstance(getattr(constants, name), type(constants)):
+                    self.assertTrue(name.isupper())
+
     def testEnums(self):
         """
         Tests to validate the enums submodule.
