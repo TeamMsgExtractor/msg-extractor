2c730f7fa4f35fed336fdaa8b033352c46f71b6b
TheElementalOfDestruction <arceusthe@gmail.com>
2023-12-17 09:10:18 -0800

    Doc updates

--- extract_msg/__init__.py
+++ extract_msg/__init__.py
@@ -27,7 +27,7 @@ https://github.com/TeamMsgExtractor/msg-extractor
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Destiny Peterson & Matthew Walker'
-__date__ = '2023-12-13'
+__date__ = '2023-12-17'
 __version__ = '0.47.1'
 
 __all__ = [
