4ee690b4e679a849c7768de17ee48583c029c75b
TheElementalOfDestruction <arceusthe@gmail.com>
2025-03-23 18:23:54 -0700

    Fix for #456

--- extract_msg/__init__.py
+++ extract_msg/__init__.py
@@ -27,8 +27,8 @@ https://github.com/TeamMsgExtractor/msg-extractor
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Destiny Peterson & Matthew Walker'
-__date__ = '2025-03-14'
-__version__ = '0.53.2'
+__date__ = '2025-03-23'
+__version__ = '0.54.0'
 
 __all__ = [
     # Modules:
